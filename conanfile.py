72a54248e21950a98e69a5f9c3cea8e8f249e7cc
deekshith-elear <45193416+deekshith-elear@users.noreply.github.com>
2019-04-29 06:58:47 +0530

    Provides conan recipe file to build artifacts (#1)

---
+++ conanfile.py
@@ -0,0 +1,28 @@
+from conans import ConanFile, CMake, tools
+
+class JanssonlibConan(ConanFile):
+    name = "jansson"
+    version = "0.1"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of janssonlib here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = { "shared": [True, False] }
+    default_options = "shared=True"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        if (self.settings.os == "Android"):
+            cmake.definitions["Platform"] = "android"
+        cmake.configure(source_folder=".")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="include")
+        self.copy("*", dst="lib", src= "lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = [ "jansson" ]
